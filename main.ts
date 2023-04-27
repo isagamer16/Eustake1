b3bf47e7ee09dfe68c77bfc9b36571ce41b1504a
isagamer16 <130647276+isagamer16@users.noreply.github.com>
2023-04-27 10:29:44 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-    basic.showString("Hola mundo")
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    Número_aleatorio = randint(0, 3)
+    if (Número_aleatorio == 2) {
+        basic.showString("SI")
+    } else if (Número_aleatorio == 1) {
+        basic.showString("NO")
+    } else {
+        basic.showString("TAL VEZ")
+    }
+    basic.showNumber(8)
 })
+let Número_aleatorio = 0
+basic.showString("haz una pregunta")
+basic.showNumber(8)
