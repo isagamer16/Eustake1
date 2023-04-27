eeb431081d242b27289766588c914fbe3c75f73a
isagamer16 <130647276+isagamer16@users.noreply.github.com>
2023-04-27 10:33:12 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
-    Número_aleatorio = randint(0, 3)
+    Número_aleatorio = randint(0, 5)
     if (Número_aleatorio == 2) {
         basic.showString("SI")
     } else if (Número_aleatorio == 1) {
         basic.showString("NO")
-    } else {
+    } else if (Número_aleatorio == 3) {
         basic.showString("TAL VEZ")
+    } else if (Número_aleatorio == 4) {
+        basic.showString("NO SE")
+    } else {
+        basic.showString("ALOMEJOR")
     }
     basic.showNumber(8)
 })
